5621d96a6a5a60b1181d49b02e619673462ad772
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2022-06-24 01:03:14 +0200

    removed edit and delete symptomes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function AddItemsToSearch() {
+   
+    const SearchLabel = document.getElementById("SearchLabel");
+
+    const li_element = document.createElement("li");
+
+    li_element.appendChild(document.createTextNode(SearchLabel.value));
+
+    document.getElementById("SearchOption").appendChild(li_element);
+    
+}
